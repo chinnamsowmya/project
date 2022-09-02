fa8cd15c01efe950cec13b2d20094d35ef207b94
Chinnam Sowmya <85185220+chinnamsowmya@users.noreply.github.com>
2022-09-02 09:03:29 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,44 @@
+package com.itc.main.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int pid;
+	private String pname;
+	private double price;
+
+	public Product() {
+
+	}
+
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public String getPname() {
+		return pname;
+	}
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
